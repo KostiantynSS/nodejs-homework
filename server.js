6d2f2f46a064156f9b0ae8daac9b62eb71ff1621
KostiantynSS <storoghenko94@gmail.com>
2023-12-04 20:27:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 
-const DB_HOST =
-  "mongodb+srv://kostiantynss:ckCF8y913rvNXxc2@cluster0.vkapukh.mongodb.net/db-contacts?retryWrites=true&w=majority";
-
 mongoose
-  .connect(DB_HOST)
+  .connect(process.env.DB_HOST)
   .then(() =>
     app.listen(4000, () => {
       console.log("Server running. Use our API on port: 4000");
