b999bb4f99a482708900c14d5efad1e233bef647
KostiantynSS <storoghenko94@gmail.com>
2023-12-14 22:16:40 +0200

    Update getAll.js

--- controller/contacts/getAll.js
+++ controller/contacts/getAll.js
@@ -5,7 +5,7 @@ const getAll = async (req, res) => {
   const { page = 1, limit = 20, favourite } = req.query;
   const limitRecords = parseInt(limit);
   const skip = (page - 1) * limit;
-  const listFavourites = favourite ? { favourite: [favourite] } : {};
+  const listFavourites = favourite ? { favourite: favourite } : {};
   const contacts = await Contact.find({ owner, ...listFavourites })
     .skip(skip)
     .limit(limitRecords);
